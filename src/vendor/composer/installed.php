f102d62a8034b98eca3768f9b67b071992ba5843
crisanchdev <anami.empresa@gmail.com>
2025-02-06 14:14:08 -0300

    conf: actualiza archivos vender

--- src/vendor/composer/installed.php
+++ src/vendor/composer/installed.php
@@ -1,13 +1,13 @@
 <?php return array(
     'root' => array(
         'name' => 'root/html',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '765c60a5d039a3f6b5a5ddd9dba3d4bce9a8cd50',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => true,
+        'dev' => false,
     ),
     'versions' => array(
         'phpmailer/phpmailer' => array(
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'root/html' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '765c60a5d039a3f6b5a5ddd9dba3d4bce9a8cd50',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
